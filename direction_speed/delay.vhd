eaa15f22b91b70436fad99b2a34edc60442890c4
dfnr-group <82193406+dfnr-group@users.noreply.github.com>
2021-05-10 23:09:11 +0700

    Update delay.vhd

--- direction_speed/delay.vhd
+++ direction_speed/delay.vhd
@@ -4,7 +4,7 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity delay is
 port(
-    RCLK          : in std_logic;
+    RCLK          : in std_logic; --anggap 10 Hz
     timerreset    : inout std_logic;
     startdelay    : in std_logic; -- dari Q pada SRFFdelay
 	 RST				: in std_logic;
@@ -13,9 +13,9 @@ end entity;
 
 architecture Behavioral of delay is
     signal ticks: integer;
-    signal seconds: integer;
+    signal seconds: integer; --atur jadi biner aja
 begin
-    process(clk, RST) is
+    process(RCLK, RST) is
     begin
 	 if RST = '1' then
 		  ticks <= 0;
@@ -25,14 +25,13 @@ begin
             ticks <= 0;
             seconds <= 0;
         elsif rising_edge(RCLK) then
-            if ticks = 25000000 - 1 then --tunggu sampai 25.000.000 tick - 1 (1 detik) (tergantung RCLK berapa Hz)
-                if seconds = 30 then --30 detik delay
+            if ticks = 9 then --10 hz
+                if seconds = 10 then --10 detik delay
                     seconds <= 0;
                     finishdelay <= '1';
-						        timerreset <= '1';
                 else
                     finishdelay <= '0';
-						        seconds <= seconds + 1;
+		    seconds <= seconds + 1;
                 end if;
                 ticks <= 0;
             else
