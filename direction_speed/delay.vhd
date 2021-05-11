9144d5688edf44f713186633c7712f4c15923f69
dfnr-group <82193406+dfnr-group@users.noreply.github.com>
2021-05-11 22:46:37 +0700

    Update delay.vhd

--- direction_speed/delay.vhd
+++ direction_speed/delay.vhd
@@ -1,41 +1,44 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
+use ieee.std_logic_unsigned.all;
 
 entity delay is
 port(
-    RCLK          : in std_logic; --anggap 10 Hz
-    timerreset    : inout std_logic;
+    RCLK          : in std_logic; -- anggap 10 Hz
+    timerreset    : in std_logic; -- reset asynchronous
     startdelay    : in std_logic; -- dari Q pada SRFFdelay
-	 RST				: in std_logic;
+	 RST				: in std_logic; -- async reset
     finishdelay   : out std_logic);
 end entity;
 
 architecture Behavioral of delay is
-    signal ticks: integer;
-    signal seconds: integer; --atur jadi biner aja
+    signal ticks: std_logic_vector(3 downto 0);
+    signal seconds: std_logic_vector(3 downto 0); --atur jadi biner aja
 begin
     process(RCLK, RST) is
     begin
 	 if RST = '1' then
-		  ticks <= 0;
-        seconds <= 0;		  
+		ticks <= "0000";
+        seconds <= "0000";
+		finishdelay <= '1';
+	 elsif timerreset = '1' then
+		ticks <= "0000";
+        seconds <= "0000";
+		  finishdelay <= '0';
     elsif startdelay = '1' then --kalo disuruh mulai oleh SRFFdelay
-        if timerreset = '1' then --kalo direset
-            ticks <= 0;
-            seconds <= 0;
-        elsif rising_edge(RCLK) then
-            if ticks = 9 then --10 hz
-                if seconds = 10 then --10 detik delay
-                    seconds <= 0;
+        if rising_edge(RCLK) then
+            if ticks = "1001" then --10 hz
+                if seconds = "1001" then --10 detik delay
+                    seconds <= "0000";
                     finishdelay <= '1';
                 else
                     finishdelay <= '0';
 		    seconds <= seconds + 1;
                 end if;
-                ticks <= 0;
+                ticks <= "0000";
             else
-                ticks <= ticks + 1; -- setiap rising edge clock akan tambah 1
+                ticks <= ticks + 1; -- 
             end if;
         end if; 
     end if;
