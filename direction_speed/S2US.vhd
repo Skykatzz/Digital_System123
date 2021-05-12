3a022267610175df5e933250706711be530b4b75
dfnr-group <82193406+dfnr-group@users.noreply.github.com>
2021-05-12 16:00:05 +0700

    Update S2US.vhd

--- direction_speed/S2US.vhd
+++ direction_speed/S2US.vhd
@@ -24,32 +24,32 @@ architecture Behavioral of S2US is
 
 
 begin
-	process(L_action,RCLK)
-	begin
-		if Rising_edge(RCLK) then
-			if L_action(8) = '1' then
-				LM_DIRECTION <= '1'; -- Mundur
-            tempL <= (not L_action) + 1; -- 9 bit
-            LM_SPEED <= tempL (7 downto 0); -- cara biar jadi 8 bit
-         elsif L_action(8) = '0' then --0 1011 1111
-            LM_DIRECTION <= '0'; -- Maju
-            LM_SPEED <= L_action (7 downto 0);  --ambil bit ke 7 to 0
-         end if; 
-		end if;
-	end process;
+process(L_action,RCLK)
+begin
+	 if Rising_edge(RCLK) then
+	    if L_action(8) = '1' then
+		LM_DIRECTION <= '1'; -- Mundur
+                tempL <= (not L_action) + 1; -- 9 bit
+                LM_SPEED <= tempL (7 downto 0); -- cara biar jadi 8 bit
+            elsif L_action(8) = '0' then --0 1011 1111
+                LM_DIRECTION <= '0'; -- Maju
+                LM_SPEED <= L_action (7 downto 0);  --ambil bit ke 7 to 0
+             end if; 
+	  end if;
+end process;
 	
-	process(R_action,RCLK)
-	begin
-		if Rising_edge(RCLK) then
-			if R_action(8) = '1' then
-				RM_DIRECTION <= '0'; -- Mundur
-            tempR <= (not R_action) + 1;
-            RM_SPEED <= tempR (7 downto 0);
-         elsif R_action(8) = '0' then --0 1011 1111
-            RM_DIRECTION <= '1'; -- Maju
-            RM_SPEED <= R_action (7 downto 0);
-         end if; 
-		end if;
-	end process;		
+process(R_action,RCLK)
+begin
+	if Rising_edge(RCLK) then
+	    if R_action(8) = '1' then
+	       RM_DIRECTION <= '0'; -- Mundur
+               tempR <= (not R_action) + 1;
+               RM_SPEED <= tempR (7 downto 0);
+           elsif R_action(8) = '0' then --0 1011 1111
+               RM_DIRECTION <= '1'; -- Maju
+               RM_SPEED <= R_action (7 downto 0);
+          end if; 
+	end if;
+end process;		
 	
 end Behavioral;
