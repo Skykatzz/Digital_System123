512672d61456620048afa283af3f9f27620a10e4
dfnr-group <82193406+dfnr-group@users.noreply.github.com>
2021-05-12 15:51:47 +0700

    Update US2S.vhd

--- direction_speed/US2S.vhd
+++ direction_speed/US2S.vhd
@@ -4,27 +4,23 @@ use ieee.numeric_std.all;
 use ieee.std_logic_unsigned.all;
 
 entity US2S is
-      generic
-      (
-              Nbits : positive := 9 
-       );
  port 
 ( 
            --Inputs
            RMF_DIRECTION : in std_logic ;
-			  RMF_SPEED : in std_logic_vector (Nbits-1 downto 0);
+			  RMF_SPEED : in std_logic_vector (7 downto 0);
 			  LMF_DIRECTION : in std_logic ;
-			  LMF_SPEED : in std_logic_vector (Nbits-1 downto 0);
+			  LMF_SPEED : in std_logic_vector (7 downto 0);
 			  RCLK: in std_logic;
            --Outputs
-           L_Feedback : out std_logic_vector (Nbits downto 0);
-			  R_Feedback : out std_logic_vector (Nbits downto 0)
+           L_Feedback : out std_logic_vector (8 downto 0);
+			  R_Feedback : out std_logic_vector (8 downto 0)
 );
 end US2S;
 
 architecture US2S_v1 of US2S is
-signal tempL : std_logic_vector(Nbits-1 downto 0);
-signal tempR : std_logic_vector(Nbits-1 downto 0);
+signal tempL : std_logic_vector(7 downto 0);
+signal tempR : std_logic_vector(7 downto 0);
 
 begin
 	process(LMF_DIRECTION,LMF_SPEED,RCLK)
@@ -33,8 +29,8 @@ begin
 		if LMF_DIRECTION = '0' then --Left 0 CW (MAJU)
 			L_Feedback <= ('0' & LMF_SPEED) ;
 		elsif LMF_DIRECTION <='1' then --Left 1 CCW (Mundur)
-			tempL <= not LMF_SPEED;
-			L_Feedback<= ('0'&(tempL + 1));
+			tempL <= (not LMF_SPEED) + 1;
+			L_Feedback<= ('1'& tempL );
 		end if;
 		end if;
 	end process ;
@@ -45,9 +41,9 @@ begin
 		if  RMF_DIRECTION ='1' then --Right 1 CCW (MAJU)
 			 R_Feedback <= ('0' & RMF_SPEED) ;
 		elsif RMF_DIRECTION ='0' then --Right 0 CW (Mundur)	
-			tempR <= not RMF_SPEED;
-			R_Feedback<= ('0'&(tempR + 1));
-		end if;
+			tempR <= (not RMF_SPEED)+1;
+			R_Feedback<= ('1'& tempR );
+		end if ;
 		end if;
 	end process ;
 
