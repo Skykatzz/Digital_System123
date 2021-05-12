0ea027a2456f0a46b8874d91e03e0954765d5d35
dfnr-group <82193406+dfnr-group@users.noreply.github.com>
2021-05-12 20:59:09 +0700

    Update SRFFdelay.vhd

--- direction_speed/SRFFdelay.vhd
+++ direction_speed/SRFFdelay.vhd
@@ -2,25 +2,25 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity SRFFdelay is
-    Port ( set : in  STD_LOGIC;
-           CLOCK : in  STD_LOGIC;
-	   RST_EN : in std_logic;
-           Q : out  STD_LOGIC;
+    Port ( SETEN : in  STD_LOGIC;
+           RCLK : in  STD_LOGIC;
+	   RSTEN : in std_logic;
+           DELAYOUT : out  STD_LOGIC;
            RST : in  STD_LOGIC);
 end SRFFdelay;
 
 architecture Behavioral of SRFFdelay is
 
 begin
-process(RST, CLOCK)
+process(RST, RCLK)
 begin
     if RST = '1' then
-        Q <= '0';
-    elsif rising_edge(CLOCK) then
-	if rising_edge(RST_EN) then
-		Q <= '0';
-        elsif set = '1' then
-		Q <= '1';
+        DELAYOUT <= '0';
+    elsif rising_edge(RCLK) then
+	if rising_edge(RSTEN) then
+		DELAYOUT <= '0';
+        elsif SETEN = '1' then
+		DELAYOUT <= '1';
         end if;
     end if;
 end process;
