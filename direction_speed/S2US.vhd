141a8e34927399e260672036dff59ce7829d8ee7
dfnr-group <82193406+dfnr-group@users.noreply.github.com>
2021-05-12 21:15:00 +0700

    Update S2US.vhd

--- direction_speed/S2US.vhd
+++ direction_speed/S2US.vhd
@@ -9,7 +9,7 @@ entity S2US is
            --Inputs DARI NICO
            L_action : in  STD_LOGIC_VECTOR (8 downto 0); -- 9 bit
            R_action : in  STD_LOGIC_VECTOR (8 downto 0);
-	   RCLK: in (std_logic);
+	   RCLK: in std_logic;
            --Outputs
            RM_DIRECTION : out std_logic ;
            RM_SPEED : out std_logic_vector (7 downto 0);
