b9a68decd3bc8f6f92def62c665f3414e5da9592
dfnr-group <82193406+dfnr-group@users.noreply.github.com>
2021-05-17 15:48:23 +0700

    Update US2S.vhd

--- direction_speed/US2S.vhd
+++ direction_speed/US2S.vhd
@@ -10,7 +10,6 @@ entity US2S is
   RMF_SPEED : in std_logic_vector (7 downto 0);
   LMF_DIRECTION : in std_logic ;
   LMF_SPEED : in std_logic_vector (7 downto 0);
-  RCLK: in std_logic;
   --Outputs
   L_Feedback : out std_logic_vector (8 downto 0);
   R_Feedback : out std_logic_vector (8 downto 0)
@@ -21,28 +20,24 @@ architecture US2S_v1 of US2S is
 signal tempL,tempR : std_logic_vector(7 downto 0);
 
 begin
-	process(LMF_DIRECTION,LMF_SPEED,RCLK)
-	begin
-		if RCLK='1' then
-		if LMF_DIRECTION = '0' then --Left 0 CW (MAJU)
-			L_Feedback <= ('0' & LMF_SPEED) ;
-		elsif LMF_DIRECTION <='1' then --Left 1 CCW (Mundur)
-			tempL <= (not LMF_SPEED) + 1;
-			L_Feedback<= ('1'& tempL );
-		end if;
-		end if;
+process(LMF_DIRECTION,LMF_SPEED)
+begin
+	if LMF_DIRECTION = '0' then --Left 0 CW (MAJU)
+	   L_Feedback <= ('0' & LMF_SPEED) ;
+	elsif LMF_DIRECTION <='1' then --Left 1 CCW (Mundur)
+	   tempL <= (not LMF_SPEED) + 1;
+	   L_Feedback<= ('1'& tempL );
+	end if;
 	end process ;
 	
-	process(RMF_DIRECTION,RMF_SPEED,RCLK)
-	begin
-		if RCLK='1' then
-		if  RMF_DIRECTION ='1' then --Right 1 CCW (MAJU)
-			 R_Feedback <= ('0' & RMF_SPEED) ;
-		elsif RMF_DIRECTION ='0' then --Right 0 CW (Mundur)	
-			tempR <= (not RMF_SPEED)+1;
-			R_Feedback<= ('1'& tempR );
-		end if ;
-		end if;
-	end process ;
+process(RMF_DIRECTION,RMF_SPEED)
+begin
+	if  RMF_DIRECTION ='1' then --Right 1 CCW (MAJU)
+	    R_Feedback <= ('0' & RMF_SPEED) ;
+	elsif RMF_DIRECTION ='0' then --Right 0 CW (Mundur)	
+	    tempR <= (not RMF_SPEED)+1;
+	    R_Feedback<= ('1'& tempR );
+	end if;
+end process ;
 
 end US2S_v1;
