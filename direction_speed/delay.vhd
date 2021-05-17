f513df5879971ec50cffdfd85bb7914ee341beba
dfnr-group <82193406+dfnr-group@users.noreply.github.com>
2021-05-17 15:24:24 +0700

    Update delay.vhd

--- direction_speed/delay.vhd
+++ direction_speed/delay.vhd
@@ -5,40 +5,30 @@ use ieee.std_logic_unsigned.all;
 
 entity delay is
 port(
-	RCLK		: in std_logic; -- anggap 10 Hz
-	timerreset	: in std_logic; -- reset asynchronous
-	startdelay	: in std_logic; -- dari Q pada SRFFdelay
+	RCLK		: in std_logic; -- 10 Hz
+	timerreset	: in std_logic; -- reset asynchronous, dari light (jika 1 --> ada cahaya)
+	startdelay	: in std_logic; -- dari DELAYOUT pada SRFFdelay
 	RST		: in std_logic; -- async reset
 	finishdelay	: out std_logic);
 end entity;
 
 architecture Behavioral of delay is
-    signal ticks: std_logic_vector(3 downto 0);
-    signal seconds: std_logic_vector(3 downto 0);
+    signal ticks: std_logic_vector(7 downto 0);
 begin
 	
 process(RCLK, RST) is
 begin
 	
-if RST = '1' then
-	ticks <= "0000";
-        seconds <= "0000";
+if RST = '1' or timerreset = '1' then
+	ticks <= "00000000";
 	finishdelay <= '1';
-elsif rising_edge(timerreset) then
-	ticks <= "0000";
-        seconds <= "0000";
-	finishdelay <= '0';
+	
 elsif startdelay = '1' and rising_edge(RCLK) then --kalo disuruh mulai oleh SRFFdelay
-	if ticks = "1001" then --10 hz
-        	if seconds = "1001" then --10 detik delay (0 sampai 9)
-                	seconds <= "0000";
-                    	finishdelay <= '1';
-                else
-                	finishdelay <= '0';
-		    	seconds <= seconds + 1;
-                end if;
-                ticks <= "0000";
+	if ticks = "01100011" then -- 100 tick (0 - 99) = 10 DETIK
+		finishdelay <= '1';
+		ticks <= "00000000";
 	else
+		finishdelay <= '0';
 		ticks <= ticks + 1;
 	end if;
 
