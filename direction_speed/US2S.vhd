7e528df002fa98dff9b0da2cdbfcf24a226caa20
dfnr-group <82193406+dfnr-group@users.noreply.github.com>
2021-05-22 10:33:46 +0700

    Update US2S.vhd

--- direction_speed/US2S.vhd
+++ direction_speed/US2S.vhd
@@ -20,21 +20,21 @@ architecture US2S_v1 of US2S is
 signal tempL,tempR : std_logic_vector(7 downto 0);
 
 begin
-process(LMF_DIRECTION,LMF_SPEED)
+process(LMF_DIRECTION,LMF_SPEED,L_Feedback,tempL)
 begin
 	if LMF_DIRECTION = '0' then --Left 0 CW (MAJU)
 	   L_Feedback <= ('0' & LMF_SPEED) ;
-	elsif LMF_DIRECTION <='1' then --Left 1 CCW (Mundur)
+	else LMF_DIRECTION <='1' then --Left 1 CCW (Mundur)
 	   tempL <= (not LMF_SPEED) + 1;
 	   L_Feedback<= ('1'& tempL );
 	end if;
 	end process ;
 	
-process(RMF_DIRECTION,RMF_SPEED)
+process(RMF_DIRECTION,RMF_SPEED,R_Feedback,tempL)
 begin
 	if  RMF_DIRECTION ='1' then --Right 1 CCW (MAJU)
 	    R_Feedback <= ('0' & RMF_SPEED) ;
-	elsif RMF_DIRECTION ='0' then --Right 0 CW (Mundur)	
+	else RMF_DIRECTION ='0' then --Right 0 CW (Mundur)	
 	    tempR <= (not RMF_SPEED)+1;
 	    R_Feedback<= ('1'& tempR );
 	end if;
