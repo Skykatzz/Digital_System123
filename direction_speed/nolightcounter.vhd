6f44ce999f301e73f2f3a2c4502c94e56d0d24fa
dfnr-group <82193406+dfnr-group@users.noreply.github.com>
2021-05-31 11:32:18 +0700

    Update nolightcounter.vhd

--- direction_speed/nolightcounter.vhd
+++ direction_speed/nolightcounter.vhd
@@ -5,7 +5,7 @@ use ieee.std_logic_unsigned.all;
 
 entity nolightcounter is
 Port (
-	RCLK : in  STD_LOGIC; -- 10 Hz
+	RCLK : in  STD_LOGIC; -- 62.5 Hz
 	RST : in  STD_LOGIC; -- asynchronous reset
 	NLC_EN : in  STD_LOGIC; -- enable dari thresholding
 	CAHAYA : in  STD_LOGIC; -- ada tidaknya cahaya
@@ -13,21 +13,21 @@ Port (
 end nolightcounter;
 
 architecture Behavioral of nolightcounter is
-	signal ticks: std_logic_vector(7 downto 0);
+	signal ticks: std_logic_vector(9 downto 0);
 	signal srl_set, srl_rst: std_logic;
 begin
 
 	process(RCLK, RST, NLC_EN, CAHAYA) is  -- PROCESS UNTUK COUNTING
 	begin
 		if RST = '1' or NLC_EN = '0' or CAHAYA = '1' then -- jika direset, belum enable, atau terdapat cahaya
-			ticks <= "00000000";
+			ticks <= "0000000000";
 		elsif rising_edge(RCLK) then
-			if ticks = "00110010" then -- 50 tick (0 - 50)
+			if ticks = "0100111001" then -- 5 detik (0 - 313)
 				srl_rst <= '1'; -- FINISH = 0 -> NEXT DIAM DI TEMPAT
 				ticks <= ticks + 1;
-			elsif ticks = "10010111" then -- 100 tick (51 - 150)
+			elsif ticks = "1110101010" then -- 10 detik (314 - 938)
 				srl_set <= '1' ; -- FINISH = 1 -> NEXT MUTER DI TEMPAT
-			   	ticks <= "00000000";
+			   	ticks <= "0000000000";
 			else
 	    		   	srl_rst <= '0';  -- kenapa tetap di 1
 				srl_set <= '0'; -- kenapa tidak berubah ke 1
