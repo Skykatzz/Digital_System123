f4e318c3b69039f7ad660fb71b494c62a4bd472d
Wilson6449 <82133433+Wilson6449@users.noreply.github.com>
2021-06-03 16:34:26 +0700

    Update ov7670_capture.vhd

--- cameracontrol&pixelcapture/ov7670_capture.vhd
+++ cameracontrol&pixelcapture/ov7670_capture.vhd
@@ -17,28 +17,29 @@
 -- Additional Comments: 
 --
 ----------------------------------------------------------------------------------
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity ov7670capture is
+entity ov7670_capture is
 Port (  pclk    : in  STD_LOGIC;
      vsync   : in  STD_LOGIC;
      href    : in  STD_LOGIC;
      data_In : in  STD_LOGIC_VECTOR (7 downto 0);
      RST    : in STD_LOGIC;
- 2clk  : out STD_LOGIC;
+     halfclk  : out STD_LOGIC;
      PixelOut: out STD_LOGIC_VECTOR (7 downto 0)
     );
      
-end ov7670capture;
+end ov7670_capture;
 
 architecture Behavioral of ov7670capture is
  signal dataY   : std_logic_vector (7 downto 0) := (others => '0');
  signal Q : std_logic := '0';
 begin
  PixelOut <= dataY;
- 2clk <= Q;
-process(pclk) 
+ halfclk <= Q;
+process(pclk,rst) 
 begin
  if RST = '1' then
   Q <= '0';
@@ -53,18 +54,17 @@ process (pclk,vsync,href,RST,Q)
  begin
    if RST = '1' then
       dataY <= (others => '0');
-   else 
-     if rising_edge(pclk)then
+   elsif  rising_edge(pclk) then
+     
        if vsync = '1' then
          dataY <= (others => '0');
-       else   
-         if href = '1' then
-           if rising_edge(Q) then 
+       elsif  href = '1' and rising_edge(Q) then 
+        
              dataY <= data_In;     
-           end if;
-         end if; 
-       end if;
+        
+        
+        end if;
      end if;
-   end if;
+   
 end process;
-end behavioral;
+end process;
