3968875bb44470d8b7e94e40daeaca99bb95c865
dfnr-group <82193406+dfnr-group@users.noreply.github.com>
2021-06-10 20:57:17 +0700

    Update master_top.vhd

--- Master_top/master_top.vhd
+++ Master_top/master_top.vhd
@@ -12,19 +12,19 @@ architecture Behavioral of master_top is -- top level masing-masing kelompok:
 -- kelompok decide speed and direction:
 component TOPLEVELSPEEDNDIR is
 Port (  -- FROM THRESHOLDING:
-		  POSITION : in  STD_LOGIC_VECTOR (9 downto 0);
-		  SIZE : in  STD_LOGIC_VECTOR (9 downto 0);
-		  CTRL_EN : in  STD_LOGIC;
-		  -- FROM MEASUREMENT:
-		  RMF_DIRECTION : in std_logic ;
-		  RMF_SPEED : in std_logic_vector (7 downto 0);
-		  LMF_DIRECTION : in std_logic ;
-		  LMF_SPEED : in std_logic_vector (7 downto 0);
-		  -- FROM CAMERA:
-		  VSYNC : in  STD_LOGIC; -- 62.5 Hz
-		  -- RESET:
-		  RST : in  STD_LOGIC;
-		  -- TO PWM:
+	POSITION : in  STD_LOGIC_VECTOR (9 downto 0);
+	SIZE : in  STD_LOGIC_VECTOR (9 downto 0);
+	CTRL_EN : in  STD_LOGIC;
+	-- FROM MEASUREMENT:
+	RMF_DIRECTION : in std_logic ;
+	RMF_SPEED : in std_logic_vector (7 downto 0);
+	LMF_DIRECTION : in std_logic ;
+	LMF_SPEED : in std_logic_vector (7 downto 0);
+	-- FROM CAMERA:
+	VSYNC : in  STD_LOGIC; -- 62.5 Hz
+	-- RESET:
+	RST : in  STD_LOGIC;
+	-- TO PWM:
         RM_DIRECTION : out std_logic ;
         RM_SPEED : out std_logic_vector (7 downto 0);
         LM_DIRECTION : out std_logic ;
@@ -71,10 +71,10 @@ port map(
 	-- RESET:
 	RST => RST,
 	-- TO PWM:
-   RM_DIRECTION => RM_DIRECTION,
-   RM_SPEED => RM_SPEED,
-   LM_DIRECTION => LM_DIRECTION,
-   LM_SPEED => LM_SPEED
+	RM_DIRECTION => RM_DIRECTION,
+	RM_SPEED => RM_SPEED,
+	LM_DIRECTION => LM_DIRECTION,
+	LM_SPEED => LM_SPEED
 );
 
 -- tulis port map masing-masing kelompok di sini:
