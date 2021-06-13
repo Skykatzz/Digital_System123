bce35c38eda58f86d770dea556b81d0c4f2208c6
ImmanuelJo <82558759+ImmanuelJo@users.noreply.github.com>
2021-06-13 22:32:31 +0700

    Update Position_Buffer.vhd

--- threshold_light_source/Position_Buffer.vhd
+++ threshold_light_source/Position_Buffer.vhd
@@ -6,6 +6,7 @@ use ieee.std_logic_unsigned.All;
 entity Position_Buffer is
     Port ( DATA : in  STD_LOGIC_VECTOR (9 downto 0);
            VSYNC : in  STD_LOGIC;
+           CLK : in STD_LOGIC;
            RST : in  STD_LOGIC;
            Pos_B : out  STD_LOGIC_VECTOR (9 downto 0));
 end Position_Buffer;
@@ -16,13 +17,15 @@ signal pos : STD_LOGIC_VECTOR (9 downto 0);
 
 begin
 
-process(DATA, VSYNC, RST) 
+process(DATA, VSYNC, RST, CLK) 
 begin
         if RST = '1' then
             pos <= (others => '0');
-        elsif VSYNC = '1' then
-            pos <= DATA;
-      end if;
+        elsif rising_edge (CLK) then
+            if VSYNC = '1' then
+                pos <= DATA;
+            end if;
+        end if;
     end process;
 
     Pos_B <= pos;
