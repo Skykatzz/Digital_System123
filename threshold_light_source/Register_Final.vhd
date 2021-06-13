5ae655fbac26828f6712520758e70206dc56e356
ImmanuelJo <82558759+ImmanuelJo@users.noreply.github.com>
2021-06-13 21:07:35 +0700

    Create Register_Final.vhd

---
+++ threshold_light_source/Register_Final.vhd
@@ -0,0 +1,37 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_arith.ALL;
+use IEEE.STD_LOGIC_unsigned.ALL;
+
+
+entity Register_Final is
+    Port ( DATA : in  STD_LOGIC_VECTOR (9 downto 0);
+           LOAD_EN : in  STD_LOGIC;
+           CLK : in  STD_LOGIC;
+           VSYNC : in  STD_LOGIC;
+           RST : in  STD_LOGIC;
+           POS : out  STD_LOGIC_VECTOR (9 downto 0));
+end Register_Final;
+
+architecture Behavioral of Register_Final is
+
+signal Final : STD_LOGIC_VECTOR (9 downto 0);
+
+begin
+
+process (RST, CLK, VSYNC, LOAD_EN, DATA)
+    begin
+        if RST = '1' then
+            Final <= (others => '0');
+        elsif rising_edge(CLK) or CLK = '1' then
+            if VSYNC = '1' then
+                Final <= (others => '0');
+            elsif LOAD_EN = '1' then
+                Final <= DATA ;
+            end if;
+        end if;
+    end process;
+
+    POS <= Final;
+
+end Behavioral;
