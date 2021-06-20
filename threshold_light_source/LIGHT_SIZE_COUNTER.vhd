6dc4aa19dd11d19312cbac873ef33ba0edf69712
ImmanuelJo <82558759+ImmanuelJo@users.noreply.github.com>
2021-06-20 15:07:49 +0700

    Update and rename Counter_Lebar.vhd to LIGHT_SIZE_COUNTER.vhd

--- threshold_light_source/LIGHT_SIZE_COUNTER.vhd
+++ threshold_light_source/LIGHT_SIZE_COUNTER.vhd
@@ -2,27 +2,19 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_unsigned.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
-entity COUNTER_LEBAR is
+entity LIGHT_SIZE_COUNTER is
     Port ( Y : in STD_LOGIC_VECTOR (7 downto 0);
            CLK : in STD_LOGIC;
            HREF : in STD_LOGIC;
            RST : in STD_LOGIC;
            YTHD : inout STD_LOGIC;
-           COUNT_LEBAR : out STD_LOGIC_VECTOR (9 downto 0));
-end COUNTER_LEBAR;
+           COUNT_LIGHT_SIZE : out STD_LOGIC_VECTOR (9 downto 0));
+end LIGHT_SIZE_COUNTER;
 
-architecture Behavioral of COUNTER_LEBAR is
+architecture Behavioral of LIGHT_SIZE_COUNTER is
 constant THD : STD_LOGIC_VECTOR (7 downto 0) := "10000000";--128
-signal lebar : STD_LOGIC_VECTOR (9 downto 0);
+signal LIGHT_SIZE : STD_LOGIC_VECTOR (9 downto 0);
 
 begin
     YTHD <= '1' when (Y >= THD)
@@ -31,15 +23,15 @@ begin
     process(RST, CLK, HREF) --Sensitivity List
     begin
         if RST = '1' then --Asynchronously
-            lebar <= (others => '0');
+            LIGHT_SIZE <= (others => '0');
         elsif rising_edge(CLK) then --Synchronously
                  if HREF = '1' AND YTHD = '1' then
-                    lebar <= lebar + 1;
+                    LIGHT_SIZE <= LIGHT_SIZE + 1;
                  elsif YTHD = '0' OR HREF = '0' then
-                    lebar <= (others => '0');
+                    LIGHT_SIZE <= (others => '0');
                  end if;
         end if;
     end process;
-    COUNT_LEBAR <= lebar;
+    COUNT_LIGHT_SIZE <= LIGHT_SIZE;
 
 end Behavioral;
