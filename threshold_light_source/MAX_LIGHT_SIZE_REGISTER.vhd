755da59c2d1ee337932acb5a6d90f20e18197e37
ImmanuelJo <82558759+ImmanuelJo@users.noreply.github.com>
2021-06-20 15:09:42 +0700

    Update and rename Register_Max.vhd to MAX_LIGHT_SIZE_REGISTER.vhd

--- threshold_light_source/MAX_LIGHT_SIZE_REGISTER.vhd
+++ threshold_light_source/MAX_LIGHT_SIZE_REGISTER.vhd
@@ -3,16 +3,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_arith.ALL;
 use IEEE.STD_LOGIC_unsigned.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
-entity REGISTER_MAX is
+entity MAX_LIGHT_SIZE_REGISTER is
     Port ( A : in std_logic_vector(9 downto 0);
            B : in std_logic_vector(9 downto 0);
            BA  : inout std_logic;
@@ -20,11 +12,11 @@ entity REGISTER_MAX is
            CLK : in STD_LOGIC;
            VSYNC : in STD_LOGIC;
            RST : in STD_LOGIC;
-           REGMAX : out STD_LOGIC_VECTOR (9 downto 0));
+           MAX_SIZE : out STD_LOGIC_VECTOR (9 downto 0));
 
-end REGISTER_MAX;
+end MAX_LIGHT_SIZE_REGISTER;
 
-architecture Behavioral of REGISTER_MAX is
+architecture Behavioral of MAX_LIGHT_SIZE_REGISTER is
 
 signal MAX : STD_LOGIC_VECTOR (9 downto 0);
 
@@ -44,5 +36,5 @@ process(DATA, RST, CLK, VSYNC) -- SENSITIVITY LIST
         end if;
     end process;
 
-    REGMAX <= MAX;
+    MAX_SIZE <= MAX;
 end Behavioral;
