12a71bbca191d986675de39d28318808634ffa8b
Wilson6449 <82133433+Wilson6449@users.noreply.github.com>
2021-06-22 19:26:08 +0700

    Update Datapath.vhd

--- cameracontrol&pixelcapture/Datapath.vhd
+++ cameracontrol&pixelcapture/Datapath.vhd
@@ -1,22 +1,4 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    14:20:42 04/23/2021 
--- Design Name: 
--- Module Name:    Datapath - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
@@ -29,32 +11,43 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 -- entity -> berhubungan dengan luar
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
--- entity -> berhubungan dengan luar
-entity i2cSender is
+entity toplevel is
     Port ( sioc : out  STD_LOGIC;
            siod : out  STD_LOGIC;
-           reset : out   STD_LOGIC;
            pwdn  : out   STD_LOGIC;
-           xclk  : out   STD_LOGIC;
-           clk : in  STD_LOGIC;
+           fsioc : in  STD_LOGIC;
            config_finished : out STD_LOGIC;
-           RST : in  STD_LOGIC);
+           pclk_in    : in  STD_LOGIC;
+           vsync   : in  STD_LOGIC;
+           href    : in  STD_LOGIC;
+           halfclk  : out STD_LOGIC;
+           PixelOut: out STD_LOGIC_VECTOR (7 downto 0);
+           d0 : in std_logic;
+           d1 : in std_logic;
+           d2 : in std_logic;
+           d3 : in std_logic;
+           d4 : in std_logic;
+           d5 : in std_logic;
+           d6 : in std_logic;
+           d7 : in std_logic;
+           mclk : out std_logic;
+           reset: out std_logic;
+           rst : in  STD_LOGIC);
            
-end i2cSender;
+end toplevel;
 
-architecture Behavioral of i2cSender is
+architecture Behavioral of toplevel is
 
+component ov7670_capture is
+    port ( pclk    : in  STD_LOGIC;
+     vsync   : in  STD_LOGIC;
+     href    : in  STD_LOGIC;
+     data_In : in  STD_LOGIC_VECTOR (7 downto 0);
+     RST    : in STD_LOGIC;
+     halfclk  : out STD_LOGIC;
+     PixelOut: out STD_LOGIC_VECTOR (7 downto 0)
+     );
+end component;
 
 component Shift_reg is
     Port ( Data : in  STD_LOGIC_VECTOR (27 downto 0);
@@ -84,7 +77,7 @@ component counter is
     Port ( CLK : in  STD_LOGIC;
            RST : in  STD_LOGIC;
            COUNT_EN : in  STD_LOGIC;
-			  stop_cond : out STD_LOGIC
+		   stop_cond : out STD_LOGIC
 			  );
 end component;
 
@@ -96,7 +89,7 @@ component Flipflop is
            sioc : out  STD_LOGIC);
 end component;
         
-        COMPONENT ov7670_registers
+        COMPONENT ov7670_registers is
 PORT(
         clk      : IN std_logic;
         advance  : IN std_logic;
@@ -114,40 +107,55 @@ END COMPONENT;
  signal internal_shift_en: STD_LOGIC;
  signal internal_set_en: STD_LOGIC; 
  signal internal_reset_en: STD_LOGIC;
- signal sys_clk  : std_logic := '0';
  signal command  : std_logic_vector(15 downto 0);
  signal finished : std_logic := '0';
  signal taken    : std_logic := '0';
  signal send     : std_logic;
  signal internal_advance: std_logic;
  signal Data_in : std_logic;
+ signal dpara : std_logic_vector(7 downto 0);
+ signal pclk : std_logic;
+ 
          constant camera_address : std_logic_vector(7 downto 0) := x"42";
 begin
-       config_finished <= finished;
-
-       reset <= '1';                                           -- Normal mode
-       pwdn  <= '0';                                           -- Power device up
-       xclk  <= sys_clk;
-       Inst_ov7670_registers: ov7670_registers PORT MAP(
-                      clk      => clk,
+   config_finished <= finished;
+   reset <= rst;                                                            -- Normal mode
+   pwdn  <= '0';                                           -- Power device up
+   pclk <= pclk_in;
+   Inst_ov7670_registers: ov7670_registers 
+   PORT MAP(
+                      clk      => fsioc,
                       advance  => internal_advance,
                       command  => command,
                       finished => finished,
                       rst   => rst
               );
-      
-       process(clk)
-       begin
-            if rising_edge(clk) then
-                              sys_clk <= not sys_clk;
-            end if;
-      end process;
+  dpara(7) <= d7;
+  dpara(6) <= d6;
+  dpara(5) <= d5;
+  dpara(4) <= d4;
+  dpara(3) <= d3;
+  dpara(2) <= d2;
+  dpara(1) <= d1;
+  dpara(0) <= d0;
+  
+    toplevel_ov7670_capture: ov7670_capture
+    port map (pclk   => pclk,
+         vsync   => vsync,
+         href    => href,
+         data_In => dpara,
+         RST    => rst,
+         halfclk  => halfclk,
+         PixelOut => pixelout
+    );
+     
+
 	Datapath_ov7670controller: ov7670_controller
 	port map (
 	Count_EN => internal_Count_En,
 	LOAD_EN => internal_load_en,
 	SHIFT_EN => internal_shift_en,
-	CLK => CLK,
+	CLK => fsioc,
 	RST => RST,
 	SET_EN => internal_SET_EN,
    RESET_EN => internal_RESET_EN,
@@ -160,13 +168,13 @@ begin
 	Data =>  internal_data,
 	LOAD_EN => internal_LOAD_EN,
 	SHIFT_EN => internal_SHIFT_EN,
-	CLK => CLK,
+	CLK => fsioc,
 	RST => RST,
 	SIOD => SIOD);
 
 	Datapath_counter: counter
 	port map(
-		   CLK => clk,
+		   CLK => fsioc,
            RST => RST,
            COUNT_EN =>internal_COUNT_EN,
 		   stop_cond => internal_stop_cond);
@@ -176,7 +184,7 @@ begin
 		   SET_EN => internal_SET_EN,
            RST    => RST,
            RESET_EN => internal_RESET_EN,
-           CLK 	=> clk,
+           CLK 	=> fsioc,
            sioc => sioc);
     
 	internal_data(27) <= '0';
