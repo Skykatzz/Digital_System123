9e28900607385b9312b78c262a14a3fdf50c598c
dfnr-group <82193406+dfnr-group@users.noreply.github.com>
2021-06-22 20:57:24 +0700

    Update nolightcounter.vhd

--- direction_speed/nolightcounter.vhd
+++ direction_speed/nolightcounter.vhd
@@ -1,3 +1,9 @@
+-- Made by: Richard Medyanto
+-- https://www.instructables.com/member/richardmedyanto/
+-- https://www.instructables.com/PART-4-the-Absence-of-Light/
+-- This module only works if light source is not detected.
+-- Remember to reset the robot when you first turn it on.
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
@@ -5,11 +11,11 @@ use ieee.std_logic_unsigned.all;
 
 entity nolightcounter is
 Port (
-	VSYNC : in  STD_LOGIC; -- 62.5 Hz
+	VSYNC : in  STD_LOGIC; -- 62.5 Hz, from clock
 	RST : in  STD_LOGIC; -- asynchronous reset
-	NLC_EN : in  STD_LOGIC; -- enable dari thresholding
-	CAHAYA : in  STD_LOGIC; -- ada tidaknya cahaya
-	FINISH : out STD_LOGIC); -- menunjukkan sudah selesai muter/diam di tempat
+	NLC_EN : in  STD_LOGIC; -- enable (or READY) from thresholding
+	CAHAYA : in  STD_LOGIC; -- availability of light
+	FINISH : out STD_LOGIC); -- shows the desired state of robot
 end nolightcounter;
 
 architecture Behavioral of nolightcounter is
@@ -17,42 +23,36 @@ architecture Behavioral of nolightcounter is
 	signal srl_set, srl_rst, Q, QBAR: std_logic;
 begin
 
-	process(VSYNC, RST, NLC_EN, CAHAYA) is  -- PROCESS UNTUK COUNTING
+	process(VSYNC, RST, NLC_EN, CAHAYA) is  -- PROCESS FOR COUNTING
 	begin
-		if RST = '1' or NLC_EN = '0' or CAHAYA = '1' then -- jika direset, belum enable, atau terdapat cahaya
+		if RST = '1' or NLC_EN = '0' or CAHAYA = '1' then -- if robot is reset, camera not ready, or light is detected
 			ticks <= "0000000000";
 			srl_rst <= '0';
-			srl_set <= '1'; -- jika cahaya = 1, modul ini diabaikan
-		else -- jika RST = '0' or NLC_EN = '1' or CAHAYA = '0'
-			if rising_edge(VSYNC) then
-				if ticks = "0100111001" then -- 5 detik (0 - 313)
-					srl_rst <= '1'; -- FINISH = 0 -> NEXT DIAM DI TEMPAT
-					ticks <= ticks + 1;
-				elsif ticks = "1110101010" then -- 10 detik (314 - 938)
-					srl_set <= '1' ; -- FINISH = 1 -> NEXT MUTER DI TEMPAT
-			   		ticks <= "0000000000";
+			srl_set <= '1';
+			-- if light is detected, ticks, srl_set, and srl_rst will set to default,
+			-- and this module is ignored.
+		else -- if RST = '0' or NLC_EN = '1' or CAHAYA = '0'
+			if rising_edge(VSYNC) then -- when the clock changes from 0 to 1
+				if ticks = "0100111001" then -- after roughly 5 seconds (0 - 313, 313 ticks/62.5 Hz = 5 seconds)
+					srl_rst <= '1'; -- resets the latch, causing FINISH => 0, tells the robot to stop moving
+					ticks <= ticks + 1; -- increment ticks again
+				elsif ticks = "1110101010" then -- after roughly 10 seconds (314 - 938)
+					srl_set <= '1' ; -- sets the latch, causing FINISH => 1, tells the robot to rotate again
+			   		ticks <= "0000000000"; -- resets the value of ticks
 				else
 	    		   		srl_rst <= '0';
-					srl_set <= '0';
-					ticks <= ticks + 1;
+					srl_set <= '0'; -- the latch will remember the latest values of the outputs
+					ticks <= ticks + 1; -- increment ticks
 				end if;
 			end if;
 		end if;
 	end process;
 	
-	-- SR LATCH:		
+	-- SR LATCH:	
 	QBAR <= srl_rst nor Q;
 	Q <= srl_set nor QBAR;
 	
 	FINISH <= QBAR;
-			
-	--process(srl_set, srl_rst) -- SR LATCH
-	--begin
-	--	if srl_set = '1' then 
-	--		FINISH <= '1'; -- DEFAULT 1, muter
-	--	elsif srl_rst = '1' then 
-	--		FINISH <= '0'; -- diam di tempat
-	--	end if;
-	--end process;
+
 	
 end Behavioral;
