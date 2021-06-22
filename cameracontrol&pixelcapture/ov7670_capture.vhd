85409008411e39ed9e6e3c307bc955b541f31871
Wilson6449 <82133433+Wilson6449@users.noreply.github.com>
2021-06-22 20:07:45 +0700

    Update ov7670_capture.vhd

--- cameracontrol&pixelcapture/ov7670_capture.vhd
+++ cameracontrol&pixelcapture/ov7670_capture.vhd
@@ -1,22 +1,5 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date  :     13:40:52 05/10/2021 
--- Design Name  :  
--- Module Name  :     ov7670capture - Behavioral 
--- Project Name :  Light Seeking Robot
--- Target Devices : 
--- Tool versions : 
--- Description  : 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
@@ -27,12 +10,13 @@ Port (  pclk    : in  STD_LOGIC;
      data_In : in  STD_LOGIC_VECTOR (7 downto 0);
      RST    : in STD_LOGIC;
      halfclk  : out STD_LOGIC;
+    
      PixelOut: out STD_LOGIC_VECTOR (7 downto 0)
     );
      
 end ov7670_capture;
 
-architecture Behavioral of ov7670capture is
+architecture Behavioral of ov7670_capture is
  signal dataY   : std_logic_vector (7 downto 0) := (others => '0');
  signal Q : std_logic := '0';
 begin
@@ -45,23 +29,23 @@ begin
  else
   if rising_edge(pclk) then
    Q <= not Q;
+  
   end if;
  end if;
 end process;
 
-process (pclk,vsync,href,RST,Q) 
+process (pclk,RST) 
  begin
    if RST = '1' then
       dataY <= (others => '0');
-   elsif  rising_edge(pclk) then
+   elsif rising_edge(pclk) then
      
-       if vsync = '1' then
-         dataY <= (others => '0');
-       elsif  href = '1' and rising_edge(Q) then 
+       if vsync = '0' then
+         if  href = '1' and q = '1' then 
         
              dataY <= data_In;     
         
-        
+            end if;
         end if;
      end if;
    
