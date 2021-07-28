dd1158cd3d6151ee5386ef7b78b8fad13db40bf8
CoderNetzach <82558912+CoderNetzach@users.noreply.github.com>
2021-07-28 18:29:47 +0700

    Update VGA_sync.vhd
    Fixed VGA sync
    Kode sudah bisa berjalan seharusnya

--- vga/VGA_sync.vhd
+++ vga/VGA_sync.vhd
@@ -4,14 +4,14 @@ use ieee.std_logic_1164.all;
 
 entity VGA_sync is
     port (
-        pclk: in  std_logic; --25MHz dari Wilson
-	rst: in  STD_LOGIC;
-        VGA_Red: out std_logic_vector(3 downto 0);
+        pclk: in std_logic;
+        rst: in  STD_LOGIC;
+        VGA_Red: out std_logic_vector (3 downto 0);
         VGA_Green: out std_logic_vector(3 downto 0);
         VGA_Blue: out std_logic_vector(3 downto 0);
-	h_sync, v_sync: out std_logic
-    );
-       
+	h_sync, v_sync: out std_logic;
+	pixelin : in std_logic_vector (7 downto 0)
+       );
 end entity VGA_sync;
  
 architecture Behavior of VGA_sync is
@@ -26,17 +26,20 @@ architecture Behavior of VGA_sync is
 	constant verticalsyncpulse : integer := 2;				 
 	constant verticalbackporch : integer := 33;       
 	
-	signal h_pos : integer := 0;
-	signal v_pos : integer := 0;
+	signal h_pos : integer ;
+	signal v_pos : integer ;
 	
-	signal videoOn : std_logic := '0';
+	signal videoOn : std_logic := '0' ;
 
 	begin
 timing:  
-    process (pclk) is
+    process (pclk, rst) is
         begin
-	if(pclk' EVENT and pclk = '1') then 
-            if h_pos < 799 then
+          if rst='1' then
+            h_pos <= 0;
+            v_pos <= 0;
+		  elsif(pclk' EVENT and pclk = '1') then 
+            if h_pos < 800 then
                 h_pos <= h_pos + 1;
             else
                 h_pos <=1;
@@ -76,8 +79,7 @@ begin
 end process;
 
 
-draw:
-process(pclk, rst, h_pos, v_pos, videoOn)
+draw:process(pclk, rst, h_pos, v_pos, videoOn, pixelin)
 begin
 	if(rst = '1')then
 		VGA_Red <= "0000";
@@ -86,25 +88,25 @@ begin
 	elsif(pclk' EVENT and pclk = '1')then
 		if(videoOn = '1')then
 			if((h_pos >= 1 and h_pos <=639) AND (v_pos >= 1 and v_pos <= 479))then
-				VGA_Red <= "1111";
-                		VGA_Green <= "1111";
-                		VGA_Blue <= "1111";
+				VGA_Red <= pixelin (7 downto 4); --test
+                		VGA_Green <= pixelin (7 downto 4);
+                		VGA_Blue <= pixelin (7 downto 4);
 			else
 				VGA_Red <= "0000";
                 		VGA_Green <= "0000";
-				VGA_Blue <= "0000";
+                		VGA_Blue <= "0000";
 			end if;
 		else
 			VGA_Red <= "0000";
             		VGA_Green <= "0000";
-            		VGA_Blue <= "0000";
+        	    	VGA_Blue <= "0000";
 		end if;
-       		  -- BLANKING
-        	if (h_pos > 1 and h_pos < 160) or (v_pos > 1 and v_pos < 40 ) then
-            		VGA_Red <= (others => '0');
-            		VGA_Green <= (others => '0');
-            		VGA_Blue <= (others => '0');
-        	end if;         
+         -- BLANKING
+        if (h_pos > 1 and h_pos < 160) or (v_pos > 1 and v_pos < 40 ) then
+            VGA_Red <= (others => '0');
+            VGA_Green <= (others => '0');
+            VGA_Blue <= (others => '0');
+        end if;         
 	end if;
 end process;
 
